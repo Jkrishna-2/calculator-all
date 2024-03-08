f32a630093ddfa0d17dec317a66b49c1a1674e92
Jkrishna-2 <jkrishna687@gmail.com>
2024-03-08 15:18:25 +0000

    calculator all

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const add = (a, b) => a + b
+exports.add = add
+const sub = (a, b) => a - b
+exports.sub = sub
+const mul = (a, b) => a * b
+exports.mul = mul
+const div = (a, b) => a / b
+exports.div = div
---
+++ index.js
@@ -0,0 +1,6 @@
+const {add, sub, mul, div} = require('./calculator')
+
+console.log(add(9, 9))
+console.log(sub(9, 9))
+console.log(mul(9, 9))
+console.log(div(36, 9))
